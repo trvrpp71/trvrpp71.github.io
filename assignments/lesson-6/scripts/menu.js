12d486260341bf39307d018ddc04927e6e9693c8
trvrpp71 <tpublic@popesonline.com>
2019-05-31 15:00:14 -0500

    first upload assignment 6

--- assignments/lesson-6/scripts/menu.js
+++ assignments/lesson-6/scripts/menu.js
@@ -2,6 +2,3 @@ function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
-function getDate() {
-    document.write(new Date().toLocaleDateString());
-}
