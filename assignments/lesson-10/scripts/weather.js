b8baadf129047fad343c2b1f7fa406e8f7bde05f
trvrpp71 <tpublic@popesonline.com>
2019-06-25 10:07:27 -0500

    api test

--- assignments/lesson-10/scripts/weather.js
+++ assignments/lesson-10/scripts/weather.js
@@ -1,8 +1,13 @@
-const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?id=2172797,&appid=e9a8fc67ba9b237ce516dd7ae7f7d335",true )
+/*const weatherObject = new XMLHttpRequest();
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?id=5604473,&appid=e9a8fc67ba9b237ce516dd7ae7f7d335",true )
 weatherObject.send();
 weatherObject.onload = function() {
     let weatherinfo=JSON.parse(weatherObject.responseText);
     console.log(weatherinfo);
 
-} //end of onload
+} //end of onload*/
+
+let weatherObject = new XMLHttpRequest();
+let apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=e9a8fc67ba9b237ce516dd7ae7f7d335';
+weatherObject.open('GET',apiURL,true);
+weatherObject.send();
