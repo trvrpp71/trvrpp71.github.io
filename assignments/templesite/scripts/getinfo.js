b4eb8c0a133b9ce4216ba642f85e63247b056ce3
trvrpp71 <tpublic@popesonline.com>
2019-07-15 14:10:59 +0200

    trying json access

--- assignments/templesite/scripts/getinfo.js
+++ assignments/templesite/scripts/getinfo.js
@@ -1,29 +1,29 @@
-function getInfo(n) {
+function getInfo() {
 
     
-    var requestURL ='https://byui-cit230.github.io/weather/data/towndata.json'
+    var requestURL ='https://github.com/trvrpp71/trvrpp71.github.io/assignments/templesite/scripts/temples.json'
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
     request.send();
 
     request.onload = function() {
-        var towns = request.response;
-        console.log(towns);
-        showTowns(towns);
+        var temple = request.response;
+        console.log(temple);
+    /*   showTemple(temple);*/
     }
 
-    function showTowns(jsonObj) {
-        var towns = jsonObj['towns'];
-        for (var i = 0; i < towns.length; i++) {
-            if (i == 1 || i == 4 || i == 5) {
-            
-            /*----get town data----------*/
+    function showTemple(jsonObj) {
+        var towns = jsonObj['temples'];
+        
                 /*----setup elements---*/
-                var section = document.querySelector('.towns');
-                var myDiv1 = document.createElement('div');
-                var myDiv2 = document.createElement('div');
-                var myH3 = document.createElement('h3');
+                var section = document.querySelector('.templeInfo');
+                var myDiv1 = document.createElement('div'); /*address*/
+                var myDiv2 = document.createElement('div'); /*phone*/
+                var myDiv3 = document.createElement('div'); /*email*/
+                var myH31 = document.createElement('h3');
+                var myH32 = document.createElement('h3');
+                var myH33 = document.createElement('h3');
                 var myPara1 = document.createElement('p');
                 var myPara2 = document.createElement('p');
                 var myPara3 = document.createElement('p');
@@ -33,42 +33,26 @@ function getInfo(n) {
                 myPara2.setAttribute("class","p2");
                 myPara3.setAttribute("class","p3");
                 myPara4.setAttribute("class","p4");
+                myH31.setAttribute("class", "th3");
+                myH32.setAttribute("class","th3");
+                myH33.setAttribute("class","th3");
                 myDiv1.setAttribute("class","container");
                 myDiv2.setAttribute("class","box");
                 
                 /*---populate elements with town data----*/
-                myH3.textContent = towns[i].name;
-                myPara1.textContent = towns[i].motto;
-                myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
-                myPara3.textContent = 'Population:  ' + towns[i].currentPopulation;
-                myPara4.textContent = 'Annual Rain Fall:  ' +towns[i].averageRainfall + '"';
+                myH31.textContent = "Address";
+                myPara1.textContent = towns[n].address;
+                myH32.textContent = "Telephone";
+                myH33.textContent = "Email";
                 
-                if (i==1){
-                    myImg.src='images/fishhavensm.jpg';
-                    myImg.setAttribute("alt","Fish Haven img");
-                } else if (i==4) {
-                    myImg.src='images/prestonsm.jpg';
-                    myImg.setAttribute("alt","Preston img");
-                } else {
-                    myImg.src='images/sodaspringssm.jpg';
-                    myImg.setAttribute("alt","Soda Springs img");
-                }
-
                 
                 /*---create the div with the elements and data---*/
-                myDiv2.appendChild(myH3);
+                myDiv2.appendChild(myH31);
+                myDiv2.appendChild(myH32);
+                myDiv2.appendChild(myH33);
                 myDiv2.appendChild(myPara1);
-                myDiv2.appendChild(myPara2);
-                myDiv2.appendChild(myPara3);
-                myDiv2.appendChild(myPara4);
-                myDiv2.appendChild(myImg);
                 myDiv1.appendChild(myDiv2);
                 section.appendChild(myDiv1);
             }
         }
-    }
-
     
-
-
-}
--- assignments/templesite/scripts/slides.js
+++ assignments/templesite/scripts/slides.js
@@ -1,6 +1,7 @@
 function currentSlide(n) {
 
     showSlides(slideIndex = n);
+    getInfo(slideIndex=n);
     }
 
 function showSlides(n) {
@@ -21,6 +22,60 @@ function showSlides(n) {
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
-function showInfo(n) {
+function getInfo(n) {
+
     
-}
+    var requestURL ='https://github.com/trvrpp71/trvrpp71.github.io/assignments/templesite/scripts/temples.json'
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        var temple = request.response;
+        console.log(temple);
+        showTemple(temple);
+    }
+
+    function showTemple(jsonObj) {
+        var towns = jsonObj['temples'];
+        
+                /*----setup elements---*/
+                var section = document.querySelector('.templeInfo');
+                var myDiv1 = document.createElement('div'); /*address*/
+                var myDiv2 = document.createElement('div'); /*phone*/
+                var myDiv3 = document.createElement('div'); /*email*/
+                var myH31 = document.createElement('h3');
+                var myH32 = document.createElement('h3');
+                var myH33 = document.createElement('h3');
+                var myPara1 = document.createElement('p');
+                var myPara2 = document.createElement('p');
+                var myPara3 = document.createElement('p');
+                var myPara4 = document.createElement('p');
+                var myImg = document.createElement('img');
+                myPara1.setAttribute("class","p1");
+                myPara2.setAttribute("class","p2");
+                myPara3.setAttribute("class","p3");
+                myPara4.setAttribute("class","p4");
+                myH31.setAttribute("class", "th3");
+                myH32.setAttribute("class","th3");
+                myH33.setAttribute("class","th3");
+                myDiv1.setAttribute("class","container");
+                myDiv2.setAttribute("class","box");
+                
+                /*---populate elements with town data----*/
+                myH31.textContent = "Address";
+                myPara1.textContent = towns[n].address;
+                myH32.textContent = "Telephone";
+                myH33.textContent = "Email";
+                
+                
+                /*---create the div with the elements and data---*/
+                myDiv2.appendChild(myH31);
+                myDiv2.appendChild(myH32);
+                myDiv2.appendChild(myH33);
+                myDiv2.appendChild(myPara1);
+                myDiv1.appendChild(myDiv2);
+                section.appendChild(myDiv1);
+            }
+        }
